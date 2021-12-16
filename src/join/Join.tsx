5c56830f8b8af3c16ae12a064c528367bb2c08b1
Jakob Hansen <jakob.hansen@hotmail.no>
2021-12-16 23:48:15 +0100

    Join session first

--- src/join/Join.tsx
+++ src/join/Join.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
+import tailwind from 'tailwind-rn';
 
 export function Join() {
-
-  return <Text>Hello World</Text>
+  return (
+    <View style={tailwind('flex-1 flex-col bg-white rounded px-8 pt-6 pb-8 justify-evenly')}>
+      <View>
+        <Text style={tailwind("text-gray-700 text-sm font-bold mb-2")}>
+        Display Name
+      </Text>
+        <TextInput
+          style={tailwind('border rounded w-full py-2 px-3 text-gray-700')}
+          placeholder="Display Name"
+        />
+      </View>
+      <View>
+        <Text style={tailwind("text-gray-700 text-sm font-bold mb-2")}>
+        Session name
+      </Text>
+        <TextInput
+          style={tailwind('border rounded w-full py-2 px-3 text-gray-700')}
+          placeholder="Session name"
+        />
+      </View>
+      <View>
+    <Button title="Join or create session" />
+      </View>
+    </View>
+  );
 }
