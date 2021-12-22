32a0fd905989824db081823df814b943e0d47800
Jakob Hansen <jakob.hansen@hotmail.no>
2021-12-22 22:17:35 +0100

    Loading bar when entering session

--- src/timer/Session.tsx
+++ src/timer/Session.tsx
@@ -1,15 +1,17 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 import {BACKEND_URL} from '@env';
 import {io} from 'socket.io-client';
 import {RootStackParamList} from '../App';
-import { PomodoroTimer } from './PomodoroTimer';
+import {PomodoroTimer} from './PomodoroTimer';
 import tailwind from 'tailwind-rn';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Session'>;
 
-type SessionState = undefined | {users : string[], clock : {timer : number, state : number}}
+type SessionState =
+  | undefined
+  | {users: string[]; clock: {timer: number; state: number}};
 
 export function Session({route, navigation}: Props) {
   const {displayName, roomName} = route.params;
@@ -24,14 +26,28 @@ export function Session({route, navigation}: Props) {
     console.log(displayName, roomName, BACKEND_URL, sessionState);
 
     return function cleanup() {
-      socket.disconnect()
-    }
+      socket.disconnect();
+    };
   }, []);
 
-  return (
-    <View>  
-      <Text style={tailwind('text-black')}>{JSON.stringify(sessionState)}</Text>
-      {sessionState && <PomodoroTimer {...sessionState.clock}></PomodoroTimer>}
-    </View>
-  );
+  function renderTimer() {
+    if (sessionState !== undefined) {
+      return (
+        <View>
+          <Text style={tailwind('text-black')}>
+            {JSON.stringify(sessionState)}
+          </Text>
+          <PomodoroTimer {...sessionState.clock}></PomodoroTimer>
+        </View>
+      );
+    } else {
+      return (
+        <View style={tailwind('flex-1 justify-center')}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+  }
+
+  return renderTimer();
 }
